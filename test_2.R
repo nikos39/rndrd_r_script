c5691f496f166875dd56befcf41a6ce36cc3bdba
Nikos Saridis <ncsaridi@gmail.com>
2024-01-08 14:19:56 +0200

    A renderable version of the test_2 script accompanied with the markdown using:
    rmarkdown::render(input = "test_2.R", output_format = "github_document")

--- test_2.R
+++ test_2.R
@@ -1,7 +1,7 @@
----
-title: "Test Drive for Git(Hub)"
-author: "nikos39"
-format: gfm
----
+#' ---
+#' title: "Test Drive for Git(Hub)"
+#' author: "nikos39"
+#' format: gfm
+#' ---
 
-This is just a test to reproduce the steps in Quarto!
+#' This is just a test to reproduce the steps in Quarto!
---
+++ test_2.md
@@ -0,0 +1,6 @@
+Test Drive for Git(Hub)
+================
+nikos39
+2024-01-08
+
+This is just a test to reproduce the steps in Quarto!
